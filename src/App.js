6d247d6f7ce0f29c23a5aa23a040f51ca8a4f0fc
swaleheen <swaleheen.jami@gmail.com>
2022-04-11 00:19:04 +0500

    error solve

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import Login from "./Pages/Login";
-import"./App.css"
+import "./App.css";
 import Dashboard from "./Pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
-return(
-  <BrowserRouter>
-        
-        <Routes>
-<<<<<<< HEAD
-            <Route path="/"  index element ={<Login/>}/>
-            <Route path="/Dashboard" element={<Dashboard/>} />
-=======
-          
-          <Route  path = "/" index element={<Login />}/>
-            
-            <Route path="/Dashboard" element={<Dashboard/>} />
-            
-        
-          
-        
->>>>>>> 65617ead2abe49978d28059c1faf3741a0a896f4
-        
-        </Routes>
-      </BrowserRouter>
-  
-  
-)
-
-};
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
